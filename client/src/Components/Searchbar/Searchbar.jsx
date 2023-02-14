07312bcca2f26ef613ac5a0568f6a597d2a0400e
AndresPM98 <andresperezmerello@MacBook-Pro-de-Andres.local>
2023-02-14 12:47:44 -0300

    Comenzamos Actions y Reducer, creamos Card

--- client/src/Components/Searchbar/Searchbar.jsx
+++ client/src/Components/Searchbar/Searchbar.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import {getClientByName} from '../../Redux/Actions';
 
 export default function Search(){
-  
+    const[input, setInput] = useState('');
+    const dispatch = useDispatch();
+
+    function handleOnChange(e){
+        setInput(e.target.value)
+    }
+    function handleOnClick(e){
+        e.preventDefault()
+        dispatch(getClientByName(input))
+    }
     return (
-         <div>
-             <button>Search</button>
-             <input type='text' placeholder='Client...'></input>
-         </div>
+        <div>
+        <button 
+        onClick={handleOnClick} className="ButtonSearch">Search</button>
+        <input 
+
+        type='text' 
+        onChange={handleOnChange} 
+        placeholder='Country...' 
+        className="InputSearch">
+
+        </input>
+    </div>
      )
 }
+
+
