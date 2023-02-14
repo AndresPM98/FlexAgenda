2584a02a6883dd397fa67e6cc0d2b529a5363d27
axelescobarr <axelescobarschneider@gmail.com>
2023-02-14 16:48:18 -0300

    Footer

---
+++ client/src/Components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css';
+
+export default function Footer(props){
+    return(
+        <div className='footerContainer'>
+            <h3 className='text'>ALL RIGHTS RESERVERD FLEXAGENDA S.A. CONTACT  0800-777-0080</h3>
+        </div>
+    )
+}
