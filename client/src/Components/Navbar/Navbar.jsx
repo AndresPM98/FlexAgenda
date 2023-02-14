65c7eeb550f3b5ead16b5ac3aaab39d658dab31e
axelescobarr <axelescobarschneider@gmail.com>
2023-02-14 16:31:26 -0300

    Navbar

---
+++ client/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import "./Navbar.css";
+import   {NavLink} from "react-router-dom";
+
+export default function (props){
+    return(
+        <div className="navbarContainer">
+            <div className="logo">
+            <iconify-icon icon="fluent-mdl2:calendar-agenda" width="25"></iconify-icon>
+            <iconify-icon icon="fluent-mdl2:gripper-bar-vertical" width="40" height="30"></iconify-icon>
+            <h1>FLEXAGENDA</h1>
+            </div>
+            <div className="buttons">
+                <NavLink to="/login" className="login">LOGIN</NavLink>
+                <NavLink to="/signUp" className="signUp">SIGN UP</NavLink>
+            </div>
+        </div>
+    )
+}
