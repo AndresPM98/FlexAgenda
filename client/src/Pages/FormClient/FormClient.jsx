e352d3ee3906579aff90f0c706347f9ac94e9828
pablo <pablo_mix_16@hotmail.com>
2023-02-15 19:23:12 -0300

    form

--- client/src/Pages/FormClient/FormClient.jsx
+++ client/src/Pages/FormClient/FormClient.jsx
@@ -1,24 +1,49 @@
 import React from "react";
-import NavbarTwo from '../../Components/NavbarTwo/NavbarTwo';
+import NavbarTwo from "../../Components/NavbarTwo/NavbarTwo";
 import Footer from "../../Components/Footer/Footer";
-import './FormClient.css';
+import "./FormClient.css";
 import { NavLink } from "react-router-dom";
 
 const FormClient = () => {
-    return(
-        <div>
-            <NavbarTwo/>
-            <div className="formClientPage">
-                <div className="backContainer">
-                    <NavLink className='back' to='/home'><iconify-icon icon="ion:arrow-back-circle" width="40" height="30"></iconify-icon>BACK</NavLink>
-                </div>
-                <div className="formClientContainer">
-                    <h1>Formulario del cliente</h1>
-                </div>
-            </div>
-            <Footer/>
+  return (
+    <div>
+      <NavbarTwo />
+      <div className="formClientPage">
+        <div className="backContainer">
+          <NavLink className="back" to="/home">
+            <iconify-icon
+              icon="ion:arrow-back-circle"
+              width="40"
+              height="30"
+            ></iconify-icon>
+            BACK
+          </NavLink>
         </div>
-    )
-}
 
-export default FormClient; 
+        <form>
+          <div className="container">
+            <div className="card">
+              <a className="signup">Formulario del cliente</a>
+              <div className="inputBox">
+                <input type="text" required />
+                <span>NOMBRE</span>
+              </div>
+              <div className="inputBox1">
+                <input type="text" required />
+                <span className="user">Email</span>
+              </div>
+              <div className="inputBox">
+                <input type="text" required />
+                <span>DNI</span>
+              </div>
+              <button className="enter">REGISTRARSE</button>
+            </div>
+          </div>
+        </form>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default FormClient;
