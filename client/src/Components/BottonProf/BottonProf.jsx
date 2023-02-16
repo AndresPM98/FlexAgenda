8cf6fe095a17a33603140e2c2842dc5c841eafe0
AndresPM98 <andresperezmerello@MacBook-Pro-de-Andres.local>
2023-02-16 12:58:06 -0300

    boton perfil profesional terminado

--- client/src/Components/BottonProf/BottonProf.jsx
+++ client/src/Components/BottonProf/BottonProf.jsx
@@ -1,37 +1,35 @@
 import { Link } from 'react-router-dom' 
 import { getProfessionalDetail } from "../../Redux/Actions";
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 const BotonProf = () => {
+  const dispatch = useDispatch(); 
+  const allTurns = useSelector((state) => state.turnBackup);
+  const profDetail = useSelector(state => state.profDetail);
 
-    const allTurns = useSelector((state) => state.turnBackup);
-    const profDetail = useSelector(state => state.profDetail)
-console.log(profDetail);
+  useEffect(() => {
+    if (allTurns.length > 0) {
+      dispatch(getProfessionalDetail(allTurns[0].professionalID));
+    }
+  }, [dispatch, allTurns]);
 
-     /* const dispatch = useDispatch(); */
-
-    useEffect(() => {
-       /*  dispatch(getProfessionalDetail(allTurns[0].professionalID))  */
-            }, [profDetail]); 
-
-/* const {name, id} = profDetail */
   return (
     <div>
-        {profDetail?
-      <div>
+      {profDetail ?
+        <div>
           <div>
-             <Link to={`/professionalDetail/${profDetail.id}`} className="nameLink"> 
-             <div>
+            <Link to={`/professionalDetail/${profDetail.id}`} className="nameLink"> 
+              <div>
                 <p>Hola {profDetail.name}</p>
-            </div>
+              </div>
             </Link> 
-           
           </div>
-      </div>: ""
-}
+        </div>
+        : ""
+      }
     </div>
-    )
+  );
 }
 
 export default BotonProf;
