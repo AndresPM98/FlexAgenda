f9abcaded6c76cd189cf43d80bbf3d20bb1cfff8
Nicolas Valdes <valdes.nicoedu@gmail.com>
2023-02-16 20:08:42 -0300

    formulario login post creado

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,35 @@
-import './App.css';
+import "./App.css";
 import { Route } from "react-router-dom";
-import { Form, Home, LandingPage, ProfessionalPage, QueryPage, FormClient } from "./Pages"
+import {
+  Form,
+  Home,
+  LandingPage,
+  ProfessionalPage,
+  QueryPage,
+  FormClient,
+  Login,
+} from "./Pages";
 
-import axios from 'axios';
-import Footer from './Components/Footer/Footer';
-axios.defaults.baseURL='https://backend-pf-production-1672.up.railway.app/'
-
+import axios from "axios";
+import Footer from "./Components/Footer/Footer";
+axios.defaults.baseURL = "https://backend-pf-production-1672.up.railway.app/";
 
 function App() {
   return (
     <div className="App">
-     
       <Route exact path="/" component={LandingPage} />
       <Route exact path="/home" component={Home} />
       <Route exact path="/form" component={Form} />
       <Route exact path="/formClient" component={FormClient} />
-      <Route exact path="/professionalDetail/:id" component={ProfessionalPage} />
+      <Route
+        exact
+        path="/professionalDetail/:id"
+        component={ProfessionalPage}
+      />
       <Route exact path="/queryDetail/:id" component={QueryPage} />
+      <Route path="/login" component={Login} />
 
-      <Footer/>
+      <Footer />
     </div>
   );
 }
