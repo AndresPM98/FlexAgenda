be416ade90b9194d41cf9d300a94a983fdf5a144
BaltaKlixJones <balta98_@hotmail.com>
2023-02-18 18:19:40 -0300

    Loading Page

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getProfClientsTurns, getTurns } from "../../Redux/Actions";
 import Cards from "../../Components/Cards/Cards"
@@ -7,9 +7,11 @@ import NavbarTwo from "../../Components/NavbarTwo/NavbarTwo";
 import BotonProf from "../../Components/BottonProf/BottonProf"; 
 import DarkMode from '../../Components/DarkMode/DarkMode'
 import './Home.css';
-
+import Loading from "../Loading/Loading";
 
 const Home = () => {
+
+    const [loading, setLoading] = useState(true)
 
     const allTurns = useSelector((state) => state.turnBackup);
     const turns = useSelector(state => state.turns);
@@ -37,21 +39,32 @@ useEffect(() => {
     dispatch(getProfClientsTurns(ultimoProfesional.id))
 },[ultimoProfesional.id])
 
-    return(
-        <div>
-            <NavbarTwo/>
 
-            <div className={darkMode == false ? 'homeContainer' : 'homeContainerDark'}>
-                <div className="filtersContainer">
-                {/* {console.log(profDetail)} */}
-                <BotonProf />
-                <Filters lastProfessional = {ultimoProfesional}/>  
-                </div>
-                <DarkMode/>
-                {profClientsTurns.length && <Cards turns = {profClientsTurns}/>}
-            </div>
+useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 1500);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div>
+      <NavbarTwo />
+
+      {/* renderizar Loading mientras loading es true */}
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className={darkMode == false ? "homeContainer" : "homeContainerDark"}>
+          <div className="filtersContainer">
+            {/* {console.log(profDetail)} */}
+            <BotonProf />
+            <Filters lastProfessional={ultimoProfesional} />
+          </div>
+          <DarkMode />
+          {profClientsTurns.length && <Cards turns={profClientsTurns} />}
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
-export default Home; 
+export default Home;
--- client/src/Pages/Professional Detail/ProfessionalPage.jsx
+++ client/src/Pages/Professional Detail/ProfessionalPage.jsx
@@ -4,9 +4,9 @@ import { NavLink, useParams } from "react-router-dom";
 
 import NavbarTwo from "../../Components/NavbarTwo/NavbarTwo";
 import { cleanProfDetail, getProfessionalDetail } from "../../Redux/Actions";
-
+import Loading from "../Loading/Loading";
 import style from "./ProfessionalPage.module.css";
-
+import { useState } from "react";
 
 import { Link } from "react-router-dom";
 
@@ -14,6 +14,9 @@ const ProfessionalPage = () => {
   const { id } = useParams();
   const professional = useSelector((state) => state.profDetail);
   const darkMode = useSelector((state) => state.darkMode);
+  const [loading, setLoading] = useState(true)
+
+
 
   const dispatch = useDispatch();
   useEffect(() => {
@@ -22,10 +25,19 @@ const ProfessionalPage = () => {
       dispatch(cleanProfDetail());
     };
   }, [dispatch, id]);
+
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 1000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  
   
   return (
     <>
       <NavbarTwo />
+      {loading? <Loading/> : 
       <div className={darkMode == false ? style.container : style.containerDark}>
         <div className={style.backContainer}>
           <NavLink className={style.back} to="/home">
@@ -41,21 +53,24 @@ const ProfessionalPage = () => {
         <p>Professional Page</p>
         <div className={style.detailContainer}>
 
+        <img style={{borderRadius:"50%", float:"left", marginRight:"10px"}} src={professional?.image ? professional.image: "https://i.stack.imgur.com/4powQ.gif" } alt="" />
         <h1 className={style.name}>{professional?.name}</h1>
         <h2 className={style.category}>{professional?.category}</h2>
         <h4 className={style.phone}>{professional?.phone}</h4>
-        <h4 className={style.adress}>{professional?.adress}</h4>
+        <h4 className={style.adress}>{professional?.address}</h4>
+        <h4 className={style.adress}>{professional?.email}</h4>
+        
         <p className={style.description}>{professional?.description}</p>
-        <img style={{borderRadius:"50%"}} src={professional?.image ? professional.image: "https://i.stack.imgur.com/4powQ.gif" } alt="" />
 
-        </div>
         <Link to={`/professional/edit/${id}`}>
         <button className={style.btnEditar}>Editar Perfil</button>
         </Link>
         <Link to={`/professional/edit/${id}/services`}>
         <button className={style.btnEditar}>Agregar Servicios</button>
         </Link>
+        </div>
       </div>
+      }
     </>
   );
 };
