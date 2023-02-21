45230bad93aa2a9b8b940933092bfe1127518404
AndresPM98 <andresperez_98@hotmail.com>
2023-02-21 11:02:04 -0300

    calendario

--- client/src/Pages/Calendar/CalendarxD.jsx
+++ client/src/Pages/Calendar/CalendarxD.jsx
@@ -9,8 +9,9 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 const locales = {
-  "en-US": require("date-fns/locale/en-US"),
+  "es": require("date-fns/locale/es"),
 };
+
 const localizer = dateFnsLocalizer({
   format,
   parse,
@@ -18,6 +19,15 @@ const localizer = dateFnsLocalizer({
   getDay,
   locales,
 });
+/* format: Una función de date-fns que se utiliza para formatear una fecha según un patrón especificado. En este caso, la función format se importa desde date-fns/format.
+
+parse: Una función de date-fns que se utiliza para analizar una cadena de fecha y devolver un objeto de fecha. En este caso, la función parse se importa desde date-fns/parse.
+
+startOfWeek: Una función de date-fns que devuelve el primer día de la semana para una fecha dada. En este caso, la función startOfWeek se importa desde date-fns/startOfWeek.
+
+getDay: Una función de date-fns que devuelve el número del día de la semana para una fecha dada. En este caso, la función getDay se importa desde date-fns/getDay.
+
+locales: Un objeto que contiene localizaciones de fecha para diferentes idiomas. En este caso, se especifica una localización para el idioma español utilizando es como clave y la localización require("date-fns/locale/es") como valor. */
 
 const events = [
   {
@@ -37,6 +47,7 @@ const events = [
     end: new Date(2023, 6, 23),
   },
 ];
+/* Array de eventos para mostrar en el calendario */
 
 function CalendarxD() {
   const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
@@ -48,17 +59,18 @@ function CalendarxD() {
       const d2 = new Date(newEvent.start);
       const d3 = new Date(allEvents[i].end);
       const d4 = new Date(newEvent.end);
-      /*
-          console.log(d1 <= d2);
+      
+          /* console.log(d1 <= d2);
           console.log(d2 <= d3);
-          console.log(d1 <= d4);
-          console.log(d4 <= d3);
-            */
+          console.log(d1 <= d4); */
+          
+            
 
       if ((d1 <= d2 && d2 <= d3) || (d1 <= d4 && d4 <= d3)) {
         alert("CLASH");
         break;
       }
+      /*  es un controlador de eventos que se ejecuta cuando se hace clic en un botón o se envía un formulario. Primero, se itera sobre todos los eventos en la lista allEvents y se comparan las fechas de inicio y finalización de los eventos con las fechas de inicio y finalización del nuevo evento que se va a agregar. Si las fechas del nuevo evento se superponen con las fechas de cualquier evento existente, se muestra una alerta y se detiene la iteración. Si no hay superposiciones, el nuevo evento se agrega a la lista de eventos existentes mediante la función setAllEvents. */
     }
 
     setAllEvents([...allEvents, newEvent]);
