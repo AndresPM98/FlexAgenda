aff4bbeaf261d2b0e254b387cc2581aaa2da388a
Nicolas Valdes <valdes.nicoedu@gmail.com>
2023-02-27 15:32:47 -0300

    Login prof terminado

--- client/src/Pages/LoginFirebase/LoginFirebase.jsx
+++ client/src/Pages/LoginFirebase/LoginFirebase.jsx
@@ -1,28 +1,30 @@
 import React, { useEffect, useState } from "react";
 import styles from "./LoginFirebase.module.css";
-import axios from "axios";
 import { useHistory } from "react-router-dom";
 import Footer from "../../Components/Footer/Footer";
 import NavbarTwo from "../../Components/NavbarTwo/NavbarTwo";
 import { useDispatch, useSelector } from "react-redux";
 
-import { auth } from "../../firebase-config";
+import { auth, userExists } from "../../firebase-config";
 
 import {
   signInWithEmailAndPassword,
-  onAuthStateChanged,
   GoogleAuthProvider,
   signInWithPopup,
+  onAuthStateChanged,
 } from "firebase/auth";
+
 import { getProfessionals } from "../../Redux/Actions";
 import Swal from "sweetalert2";
+import AuthProvider from "../../Components/AuthProvider/AuthProvider";
 
 const LoginFirebase = () => {
   const history = useHistory();
+  const dispatch = useDispatch();
   const darkMode = useSelector((state) => state.darkMode);
   const professionals = useSelector((state) => state.allProfessionals);
-  const dispatch = useDispatch();
-  const [currentUser, setCurrentUser] = useState(null);
+
+  const [state, setCurrentState] = useState(null);
   const [form, setForm] = useState({
     email: "",
     password: "",
@@ -30,36 +32,31 @@ const LoginFirebase = () => {
 
   useEffect(() => {
     dispatch(getProfessionals());
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      setCurrentUser(user);
-    });
-
-    return () => {
-      unsubscribe();
-    };
-  }, []);
+  }, [dispatch]);
 
   const changeHandler = (event) => {
     const property = event.target.name;
     const value = event.target.value;
 
     setForm({ ...form, [property]: value });
-    // validate({ ...form, [property]: value });
   };
-  const handleSignInWithGoogle = async () => {
-    const googleProvider = new GoogleAuthProvider();
 
-    const signInWithGoogle = async (googleProvider) => {
-      try {
-        const res = await signInWithPopup(auth, googleProvider).then(() => {
-          history.push(`/`);
-        });
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    await signInWithGoogle(googleProvider);
-  };
+  // const handleSignInWithGoogle = async () => {
+  //   const googleProvider = new GoogleAuthProvider();
+
+  //   const signInWithGoogle = async (googleProvider) => {
+  //     try {
+  //       const res = await signInWithPopup(auth, googleProvider).then(() => {
+  //         // history.push(`/`);
+  //         console.log("se ha logueado con google correctamente");
+  //       });
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+  //   setCurrentState(true);
+  //   await signInWithGoogle(googleProvider);
+  // };
 
   const submitHandler = async (event) => {
     event.preventDefault();
@@ -70,65 +67,90 @@ const LoginFirebase = () => {
         form.email,
         form.password
       );
-      await Swal.fire({
-        title: "Registro exitoso",
-        icon: "success",
-        text: "El usuario ha sido registrado correctamente.",
-        confirmButtonText: "Aceptar",
-      }).then(() => {
-        const findProf = professionals.find(
-          (prof) => prof.email === form.email
-        );
-        history.push(`/home/${findProf.id}`);
-      });
     } catch (error) {
       console.error(error);
-      // Aquí puedes manejar el error de inicio de sesión, como mostrar un mensaje de error al usuario
     }
   };
-  return (
-    <div>
-      <NavbarTwo />
-      <div
-        className={darkMode === false ? styles.container : styles.containerDark}
-      >
-        <form onSubmit={submitHandler} className={styles.form}>
-          <h1 className={styles.tittle}>LOGIN</h1>
+  const handleUserLoggedIn = async (id) => {
+    // redirigir a home
+    await Swal.fire({
+      title: "Registro exitoso",
+      icon: "success",
+      text: "El usuario ha sido registrado correctamente.",
+      confirmButtonText: "Aceptar",
+    }).then(() => {
+      history.push(`/Calendarpage/${id}`);
+    });
+  };
+  const handleUserNotLoggedIn = () => {
+    setCurrentState(1);
+  };
+  const handleUserNotRegistered = () => {};
 
-          <label className={styles.label}>EMAIL:</label>
-          <input
-            className={styles.input}
-            type="text"
-            required
-            value={form.email}
-            onChange={changeHandler}
-            name="email"
-          />
+  if (state === 1) {
+    return (
+      <div>
+        <NavbarTwo />
+        <div
+          className={
+            darkMode === false ? styles.container : styles.containerDark
+          }
+        >
+          <form onSubmit={submitHandler} className={styles.form}>
+            <h1 className={styles.tittle}>LOGIN</h1>
 
-          <label className={styles.label}>PASSWORD:</label>
-          <input
-            className={styles.input}
-            type="password"
-            required
-            value={form.name}
-            onChange={changeHandler}
-            name="password"
-          />
-
-          <button type="submit" className={styles.button}>
-            LOGIN
-          </button>
-          <button className={styles.googlebtn} onClick={handleSignInWithGoogle}>
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-              alt="Google logo"
+            <label className={styles.label}>EMAIL:</label>
+            <input
+              className={styles.input}
+              type="text"
+              required
+              value={form.email}
+              onChange={changeHandler}
+              name="email"
             />
-            Sign in with Google
-          </button>
-        </form>
+
+            <label className={styles.label}>PASSWORD:</label>
+            <input
+              className={styles.input}
+              type="password"
+              required
+              value={form.name}
+              onChange={changeHandler}
+              name="password"
+            />
+
+            <button type="submit" className={styles.button}>
+              LOGIN
+            </button>
+            {/* <button
+              className={styles.googlebtn}
+              onClick={handleSignInWithGoogle}
+            >
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                alt="Google logo"
+              />
+              Sign in with Google
+            </button> */}
+          </form>
+        </div>
+        <Footer></Footer>
       </div>
-      <Footer></Footer>
-    </div>
+    );
+  }
+
+  return (
+    <>
+      <NavbarTwo />
+      {/* implementar un mejor loading */}
+      <AuthProvider
+        onUserLoggedIn={handleUserLoggedIn}
+        onUserNotLoggedIn={handleUserNotLoggedIn}
+        onUserNotRegistered={handleUserNotRegistered}
+      >
+        <div>loading...</div>
+      </AuthProvider>
+    </>
   );
 };
 
