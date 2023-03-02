cfe329e2daf996f9b3a8c863abc15b061ab10843
BaltaKlixJones <balta98_@hotmail.com>
2023-03-02 16:47:07 -0300

    icono de cerrar sesion

--- client/src/Components/NavbarTwo/NavbarTwo.jsx
+++ client/src/Components/NavbarTwo/NavbarTwo.jsx
@@ -1,10 +1,34 @@
 import style from "./NavbarTwo.module.css";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 import {Link } from "react-router-dom";
+import { auth } from "../../firebase-config";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { useHistory } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+
 
 export default function NavbarTwo(props){
 
     const darkMode = useSelector((state) => state.darkMode)
+    const [currentUser, setCurrentUser] = useState(null);
+    const history = useHistory();
+    const location = useLocation();
+    
+
+    const handleLogout = async () => {
+        try {
+          await signOut(auth);
+          
+          setCurrentUser(null)
+          history.push("/")
+        } catch (error) {
+          // Manejar el error
+          console.error(error);
+        }
+      };
+
+ 
 
     return(
         <div className={!darkMode? style.navbarContainer : style.navbarContainerDark}>
@@ -15,6 +39,11 @@ export default function NavbarTwo(props){
                 </Link>
                 <h1>FLEXAGENDA</h1>
             </div>
+{location.pathname === '/Login' || location.pathname === '/SignUp'|| location.pathname === "/allProfessionals" ? null :
+                <button style={{ height:"40px", marginTop:"20px", cursor:"pointer"}}onClick={handleLogout} className="allProfessionals">
+            CERRAR SESION
+          </button>
+          }
         </div>
     ) 
 }
