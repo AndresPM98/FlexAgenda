3632ea03c1bc8148771c49030d0f669cc32de7ed
BaltaKlixJones <balta98_@hotmail.com>
2023-03-03 11:58:45 -0300

    Reviews en pagina de admin arreglado

--- client/src/Components/Admin/CardsAdmin/CardsReviewsAdmin.jsx
+++ client/src/Components/Admin/CardsAdmin/CardsReviewsAdmin.jsx
@@ -6,35 +6,43 @@ import { Link } from "react-router-dom";
 import style from "../Admin.module.css";
 import { useState } from "react";
 import Loading from "../../../Pages/Loading/Loading";
-import axios  from "axios";
+import axios from "axios";
 
-
-export default function CardsReviewsAdmin() {
+export default function CardsReviewsAdmin({ id }) {
   const allProfessionals = useSelector((state) => state.allProfessionals);
   const dispatch = useDispatch();
-const [loading, setLoading] = useState(true);
-  console.log(
-    allProfessionals.map((professional) =>
-      professional.name === "ADMIN"
-        ? ""
-        : professional.review.map((review) => review == null ? review= "" : "")
-    )
-  );
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     dispatch(getProfessionals()).then(() => setLoading(false));
   }, []);
 
-  
-
   if (loading) return <Loading />;
 
-  const reviews = allProfessionals
-  .flatMap((professional) => professional.review ?? []) 
-  .map((review) => review === null ? { score: 0 } : review);  
+  const reviewsByProfessional = allProfessionals.reduce((acc, professional) => {
+    if (professional.name !== "ADMIN") {
+      const reviews = professional.review ?? [];
+      if (acc[professional.id]) {
+        acc[professional.id].push(...reviews);
+      } else {
+        acc[professional.id] = reviews;
+      }
+    }
+    return acc;
+  }, {});
 
-  const totalScores = reviews.reduce((acc, review) => acc + parseInt(review.score), 0); 
-  const averageScore = totalScores / reviews.length;
+  const professionalScores = {};
+
+  for (const id in reviewsByProfessional) {
+    const reviews = reviewsByProfessional[id];
+    const totalScores = reviews.reduce(
+      (acc, review) => acc + parseInt(review.score),
+      0
+    );
+    const averageScore = Math.ceil(totalScores / reviews.length).toFixed(2);
+
+    professionalScores[id] = averageScore;
+  }
 
   return (
     <div className={style.adminpage}>
@@ -57,24 +65,39 @@ const [loading, setLoading] = useState(true);
       <h1>Reviews</h1>
       <div className={style.cardcontainer}>
         {allProfessionals.map((professional) =>
-          professional.name === "ADMIN" ? "" : (
+          professional.name === "ADMIN" ? (
+            ""
+          ) : (
             <div
               className={style.cardProf}
               style={{ width: "400px", height: "400px", overflow: "scroll" }}
             >
               <h2>{professional.name}</h2>
-              <h3>Promedio: {averageScore} ⭐️</h3>
+              <h3>
+                Promedio:{" "}
+                {isNaN(professionalScores[professional.id])
+                  ? 0
+                  : professionalScores[professional.id]}{" "}
+                ⭐️
+              </h3>
+              <h3>
+                Reviews:{" "}
+                {professional.review ? professional.review.length : "0"}
+              </h3>
               <h3>
                 <hr />
-                {professional.review.map((review) => (
-                  review === null ? review = "" :
-                  <div>
-                    ⭐️ {review.score}: {review.text} 
-                    
-                    <br />
-                    <br />
-                  </div>
-                ))}
+                {professional.review ? (
+                  professional.review.map((review) => (
+                    <div>
+                      <h4>
+                        ⭐️ {review.score ? review.score : ""}:{" "}
+                        {review.text ? review.text : ""}
+                      </h4>
+                    </div>
+                  ))
+                ) : (
+                  <div>Todavia no hay devoluciones</div>
+                )}
               </h3>
             </div>
           )
