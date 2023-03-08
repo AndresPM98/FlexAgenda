4541f9c07287c8c2312e21efce845c7cd8542511
FidoDido <fidel.romano.2016@hotmail.com>
2023-03-07 21:18:34 -0300

    css dashboard admin

--- client/src/Components/Admin/CardsAdmin/CardsProfAdmin.jsx
+++ client/src/Components/Admin/CardsAdmin/CardsProfAdmin.jsx
@@ -1,5 +1,9 @@
 import React from "react";
-import { getProfessionals, deleteProfessional, deleteTurn } from "../../../Redux/Actions";
+import {
+  getProfessionals,
+  deleteProfessional,
+  deleteTurn,
+} from "../../../Redux/Actions";
 import style from "../Admin.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
@@ -18,9 +22,9 @@ export default function CardsAdminProf({ id }) {
   const history = useHistory();
   const allPorfessionals = useSelector((state) => state.allProfessionals);
   const turns = useSelector((state) => state.turnBackup);
-  const allProfOrd= allPorfessionals.sort((a, b) => (a.name > b.name ? 1 : -1))
-
-
+  const allProfOrd = allPorfessionals.sort((a, b) =>
+    a.name > b.name ? 1 : -1
+  );
 
   const dispatch = useDispatch();
 
@@ -29,27 +33,23 @@ export default function CardsAdminProf({ id }) {
   }, []);
 
   const [currentUser, setCurrentUser] = useState(null);
-  
- 
 
   const handleLogout = async () => {
     try {
       await signOut(auth);
-      
-      setCurrentUser(null)
+
+      setCurrentUser(null);
       await Swal.fire({
         icon: "success",
         title: "Sesion cerrada",
         showConfirmButton: false,
         timer: 1500,
       });
-      history.push("/")
+      history.push("/");
     } catch (error) {
-     
       console.error(error);
     }
   };
-  
 
   const handlerDelete = async (id) => {
     const confirmDelete = await Swal.fire({
@@ -60,13 +60,17 @@ export default function CardsAdminProf({ id }) {
       confirmButtonColor: "#3085d6",
       cancelButtonColor: "#d33",
       confirmButtonText: "Sí, eliminar",
-      cancelButtonText: "Cancelar"
+      cancelButtonText: "Cancelar",
     });
     if (confirmDelete.isConfirmed) {
-      const turnIdsToDelete = turns.filter(turn => turn.professionalID === id).map(turn => turn.id);
-      const deleteTurnPromises = turnIdsToDelete.map(turnId => dispatch(deleteTurn(turnId)));
-      await Promise.all(deleteTurnPromises); 
-      dispatch(deleteProfessional(id))
+      const turnIdsToDelete = turns
+        .filter((turn) => turn.professionalID === id)
+        .map((turn) => turn.id);
+      const deleteTurnPromises = turnIdsToDelete.map((turnId) =>
+        dispatch(deleteTurn(turnId))
+      );
+      await Promise.all(deleteTurnPromises);
+      dispatch(deleteProfessional(id));
       await Swal.fire({
         icon: "success",
         title: "Profesional eliminado",
@@ -75,7 +79,6 @@ export default function CardsAdminProf({ id }) {
       });
       window.location.reload();
       dispatch(getProfessionals());
-      ;
     }
   };
 
@@ -86,26 +89,26 @@ export default function CardsAdminProf({ id }) {
   const handlerEdit = async (id) => {
     try {
       const confirmEdit = await Swal.fire({
-        title: '¿Estás seguro de que deseas deshabilitar este profesional?',
-        icon: 'warning',
+        title: "¿Estás seguro de que deseas deshabilitar este profesional?",
+        icon: "warning",
         showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Sí, deshabilitar',
-        cancelButtonText: 'Cancelar'
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Sí, deshabilitar",
+        cancelButtonText: "Cancelar",
       });
-  
+
       if (confirmEdit.isConfirmed) {
         await axios.put(`/professional/${id}`, { disponibility: false });
         setDisponibilityStatus({ disponibility: false });
-  
+
         await Swal.fire({
-          title: 'Profesional deshabilitado',
-          icon: 'success',
+          title: "Profesional deshabilitado",
+          icon: "success",
           showConfirmButton: false,
-            timer: 1500,
+          timer: 1500,
         });
-  
+
         window.location.reload();
       }
     } catch (error) {
@@ -125,15 +128,16 @@ export default function CardsAdminProf({ id }) {
         confirmButtonText: "Sí, habilitar",
         cancelButtonText: "Cancelar",
       });
-  
+
       if (confirmEdit.isConfirmed) {
         await axios.put(`/professional/${id}`, { disponibility: true });
         setDisponibilityStatus({ disponibility: true });
         await Swal.fire({
-          title: 'Profesional habilitado',
-          icon: 'success',
-           showConfirmButton: false,
-        timer: 1500});
+          title: "Profesional habilitado",
+          icon: "success",
+          showConfirmButton: false,
+          timer: 1500,
+        });
         window.location.reload();
       }
     } catch (error) {
@@ -145,54 +149,59 @@ export default function CardsAdminProf({ id }) {
     return <Loading />;
   }
 
-
-  const disabledProfessionals = allProfOrd.filter((professional) => !professional.disponibility);
-  const enabledProfessionals = allProfOrd.filter((professional) => professional.disponibility);
-  const combinedProfessionals = [...disabledProfessionals, ...enabledProfessionals];
+  const disabledProfessionals = allProfOrd.filter(
+    (professional) => !professional.disponibility
+  );
+  const enabledProfessionals = allProfOrd.filter(
+    (professional) => professional.disponibility
+  );
+  const combinedProfessionals = [
+    ...disabledProfessionals,
+    ...enabledProfessionals,
+  ];
 
   return (
     <div className={style.adminpage}>
-      <h1>Dashboard admin  
-        </h1>
+      <h1>Dashboard admin</h1>
+      <div>
         <div style={{ position: "absolute", top: 0, left: 50 }}>
-  <Link to={`/admin/16aa4db8-b8cf-43bf-989a-5c7945212080`}>
-    <img
-      style={{ width: "60px", height: "60px", marginTop: "10px" }}
-      src="https://cdn-icons-png.flaticon.com/512/25/25694.png"
-      alt=""
-    />
-    
-  </Link>
-    </div>
+          <Link to={`/admin/16aa4db8-b8cf-43bf-989a-5c7945212080`}>
+            <img
+              style={{ width: "60px", height: "60px", marginTop: "10px" }}
+              src="https://cdn-icons-png.flaticon.com/512/25/25694.png"
+              alt=""
+            />
+          </Link>
+        </div>
 
-  <div style={{ position: "absolute", top: 0, right: 30 }}>
-  <Link to={`/`} onClick={handleLogout}>
-    <img
-      style={{ width: "50px", height: "50px", marginTop: "10px" }}
-      src="https://cdn-icons-png.flaticon.com/512/6437/6437583.png"
-      alt=""
-    />
-    
-  </Link>
-</div>
-   
-      
+        <div style={{ position: "absolute", top: 0, right: 30 }}>
+          <Link to={`/`} onClick={handleLogout}>
+            <img
+              style={{ width: "50px", height: "50px", marginTop: "10px" }}
+              src="https://cdn-icons-png.flaticon.com/512/6437/6437583.png"
+              alt=""
+            />
+          </Link>
+        </div>
 
-      <Link
-        to={`/allProfessionalsDashboardAdmin/36d12c77-50ae-4f7c-914b-21a53f82eaab`}
-      >
-        <button className={style.adminbutton}>Profesionales</button>
-      </Link>
-      <Link
-        to={`/allClientsDashboardAdmin/36d12c77-50ae-4f7c-914b-21a53f82eaab`}
-      >
-        <button className={style.adminbutton}>Clientes</button>
-      </Link>
-      <Link to={`/allReviewsDashboardAdmin/36d12c77-50ae-4f7c-914b-21a53f82eaab`}>
-        <button className={style.adminbutton}>Reviews</button>
-      </Link>
+        <Link
+          to={`/allProfessionalsDashboardAdmin/36d12c77-50ae-4f7c-914b-21a53f82eaab`}
+        >
+          <button className={style.adminbutton}>Profesionales</button>
+        </Link>
+        <Link
+          to={`/allClientsDashboardAdmin/36d12c77-50ae-4f7c-914b-21a53f82eaab`}
+        >
+          <button className={style.adminbutton}>Clientes</button>
+        </Link>
+        <Link
+          to={`/allReviewsDashboardAdmin/36d12c77-50ae-4f7c-914b-21a53f82eaab`}
+        >
+          <button className={style.adminbutton}>Reviews</button>
+        </Link>
+      </div>
 
-      <h2 >Profesionales ({allProfOrd.length})</h2>
+      <h2>Profesionales ({allProfOrd.length})</h2>
       <div className={style.cardcontainer} style={{ display: "flex" }}>
         {combinedProfessionals.map((professional, index) => (
           <div className={style.cardProf}>
